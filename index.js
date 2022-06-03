5956bae06d56f58886c1608a453dc6c6bee7c28f
Dhithen <calvaroj@uninorte.edu.co>
2022-06-03 16:43:42 -0500

    create first table

--- index.js
+++ index.js
@@ -4,19 +4,22 @@ const port = 3000
 const MongoClient = require('mongodb').MongoClient
 
 // Connection URL
-const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/db';
 
 app.get('/', (req, res) => {
   MongoClient.connect(mongoURL, { useNewUrlParser: true }, (err, db) => {
     if (err) {
       res.status(500).send('NOTOK');
-      console.log('notok')
     } else {
-      res.send('OK! 😎');
-      console.log('ok')
+      var dbo = db.db("db");
+      dbo.createCollection("tablaA", function(err, res) {
+      if (err) throw err;
+      console.log("Collection created!");
       db.close();
+    });
     }
   });
+  var db = MongoClient.connection;
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
