4cb2dcce1a3566584f8d595ba415a6936bc8fadc
calvaroj <calvaroj@uninorte.edu.co>
2022-06-03 23:42:31 -0500

    Actualizadas funciones, falta verificar el hash

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const port = 3000
 const MongoClient = require('mongodb').MongoClient
 
 // Connection URL process.env.MONGO_URL || 
-const mongoURL = 'mongodb://localhost:27017/db';
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/db';
 
 app.get('/', (req, res) => {
   MongoClient.connect(mongoURL, { useNewUrlParser: true }, (err, db) => {
     if (err) {
-      res.status(500).send('NOTOK');
+      res.status(500).send('NOK');
       console.log(err)
     } else {
       var dbo = db.db("db");
@@ -28,8 +28,7 @@ app.get('/', (req, res) => {
 app.get('/:IDES',(req,res)=>{
   MongoClient.connect(mongoURL, { useNewUrlParser: true }, (err, db) => {
     if (err) {
-      res.status(500).send('NOTOK');
-      console.log('notok')
+      res.status(500).send('NOK');
     } else {
       let id=req.params.IDES
       let date = Date.now()
@@ -49,45 +48,65 @@ app.get('/:IDES',(req,res)=>{
 app.get('/:IDESA/:HASH',(req, res)=>{
   MongoClient.connect(mongoURL, { useNewUrlParser: true }, (err, db) => {
     if (err) {
-      res.status(500).send('NOTOK'); 
-      console.log('notok')
+      res.status(500).send('NOK'); 
     } else {
       var dbo = db.db("db");
       let id=req.params.IDESA
       let hash = req.params.HASH
-      var query = { id: id, hash:hash };
+      let date = Date.now()
+      let ans =""
+      var query = { ID: id.toString(), hash:hash.toString() };
       dbo.collection("tablas").find(query).toArray(function(err, result) {
         if (err){
           res.send('problema con la busqueda')
         }else{
           if(result.length!=0){
             console.log(result)
-            res.send('encontrado')
+            res.send('ok')
+            ans='ok'
           }else{
             console.log(result)
-            res.send('no encontrado')
+            res.send('nok')
+            ans='nok'
           }
-          
         }
       });
+      var myobj={ID:id, timestamp:date, hash:hash, res:ans}
+      dbo.collection("tablaa").insertOne(myobj, function(err, res) {
+        if (err) console.log(err);
+        });
+        console.log("Insertado", myobj)
       //db.close();
     }
   });
 });
 
 
-app.get('/:p/:t/:s',(req,res)=>{
+app.get('/p/t/s',(req,res)=>{
   MongoClient.connect(mongoURL, {useNewUrlParser:true},(err,db)=>{
     if(error){
       res.send ('La tabla no se ha creado')
     }else{
-      let tabla=req.params.s
       var dbo= db.db("db")
-      console.log(tabla)
-      dbo.collection(tabla).find({}).toArray(function(err, result) {
+      dbo.collection('tablaa').find({}).toArray(function(err, result) {
         if (err) console.log(err);
+        console.log('Tabla A',result)
         res.send(result)
       });
+      dbo.collection('tablas').find({}).toArray(function(err, result) {
+        if (err) console.log(err);
+        console.log('Tabla S',result)
+        res.send(result)
+      });
+      dbo.collection("tablaa").drop(function(err, delOK) {
+        if (err) throw err;
+        if (delOK) console.log("Collection deleted");
+      }); 
+      dbo.collection("tablas").drop(function(err, delOK) {
+        if (err) throw err;
+        if (delOK) console.log("Collection deleted");
+        db.close();
+      }); 
     }
   })
 });
